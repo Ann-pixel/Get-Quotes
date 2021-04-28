01047e4a8b6c1d9d339978c936725c90efe3ee78
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-04-28 08:11:21 -0400

    added function to fetch from api and display quote

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 // alert("Baaaaaaaaaaaaa! Youre on! 🎇");
-console.log("Hi there!");
+// console.log("Hi there!");
+
+//GET QUOTES from api
+let apiQuotes = [];
+
+function newQuote() {
+  const length = apiQuotes.length;
+  const idx = Math.floor(Math.random() * length);
+  const quote = apiQuotes[idx];
+  console.log(quote);
+}
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const res = await fetch(apiUrl);
+    apiQuotes = await res.json();
+    newQuote();
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+//On load
+getQuotes();
